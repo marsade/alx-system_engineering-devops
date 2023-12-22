0f9e6f29014a3eb2fa1c9ceeecff1ab86532c3ed
marsade <adenijifara@gmail.com>
2023-12-22 20:26:31 +0200

    Task complete

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+# Ensure flask is installed from pip3
+
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path => ['/usr/bin/'],
+  require => Package['python3-pip']
+}
+
