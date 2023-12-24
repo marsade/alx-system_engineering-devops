a5f30c01c42817cd20a35a150332de842ccebf57
marsade <adenijifara@gmail.com>
2023-12-24 11:08:33 +0200

    add flask and werkzeung packages

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,20 @@
-#!/usr/bin/pup
-# Ensure flask is installed from pip3
+#!/usr/bin/puppet
 
-package { 'flask':
+# Ensure python3-pip is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Ensure Flask is installed with version 2.1.0
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+# Ensure Werkzeug is installed with version 2.1.1
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
