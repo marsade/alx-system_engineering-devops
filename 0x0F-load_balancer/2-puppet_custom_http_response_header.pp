215178288ebd66402662ba462313ca3b893489db
marsade <adenijifara@gmail.com>
2024-01-13 16:22:05 +0200

    Task complete

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,8 +7,7 @@ package { 'nginx':
 file_line { 'add header':
   ensure  => 'present',
   after   => 'server_name _;'
-  line    => '      add_header X-Served-By $HOSTNAME;',
-  path    => '/etc/nginx/sites-available/default',
+  line   => "      add_header X-Served-By ${facts['hostname']};",  path    => '/etc/nginx/sites-available/default',
 }
 
 file { '/var/www/html/index.html':
