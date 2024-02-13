e8c7933339e7dbb3f3c28f0b4f0823c0d59f05bf
marsade <adenijifara@gmail.com>
2024-02-13 18:32:09 +0200

    Task complete

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,4 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 """
 This module queries the Reddit API for the number of subscriptions
 for a given subreddit
@@ -7,4 +7,15 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    pass
+    headers = {'User-Agent': 'RedditSubs'}
+    subreddit = subreddit
+    url = 'https://www.reddit.com/r'
+    full_url = '{}/{}/about.json'.format(url, subreddit)
+    response = requests.get(full_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers_count = data['data']['subscribers']
+        return subscribers_count
+    else:
+        return 0
