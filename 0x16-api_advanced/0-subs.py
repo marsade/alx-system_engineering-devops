ef45d4cb5639ec883f37d537f48c08ee60524ab6
marsade <adenijifara@gmail.com>
2024-02-13 18:10:20 +0200

    Initial commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,10 @@
+#/usr/bin/python3
+"""
+This module queries the Reddit API for the number of subscriptions
+for a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    pass
